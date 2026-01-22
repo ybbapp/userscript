d8c46b65f19cad2e46212c469a868d3695d31e7c
@刘羡鱼 <evlicn.ok@gmail.com>
2026-01-22 07:30:26 +0000

    chore: update match

--- feishu/browser-compatible/user.js
+++ feishu/browser-compatible/user.js
@@ -7,6 +7,7 @@
 // @match        https://*.feishu.cn/docx/*
 // @match        https://*.feishu.cn/drive/*
 // @match        https://*.feishu.cn/wiki/*
+// @match        https://*.feishu.cn/base/*
 // @icon         https://www.feishu.cn/favicon.ico
 // @license      MIT
 // @downloadURL https://github.com/ybbapp/userscript/raw/refs/heads/main/feishu/browser-compatible/user.js
@@ -16,7 +17,7 @@
 (function() {
     'use strict';
     waitForElm('.not-compatible__announce').then(dom => {
-        // dom.querySelector('.not-compatible__announce .ud__notice__close').click();
+        // <div class="not-compatible__announce disabled-contextmenu"><div class="ud__notice ud__notice-warning ud__notice-align-center ud__notice-closable ud__notice-banner ud__notice-showIcon ud__notice--filled" role="alert"><span class="universe-icon ud__notice__statusIcon"><svg width="1em" height="1em" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" data-icon="WarningFilled"><path d="M23 12c0 6.075-4.925 11-11 11S1 18.075 1 12 5.925 1 12 1s11 4.925 11 11ZM12 7a1 1 0 0 0-1 1v5a1 1 0 1 0 2 0V8a1 1 0 0 0-1-1Zm0 8a1 1 0 1 0 0 2 1 1 0 0 0 0-2Z" fill="currentColor"></path></svg></span><div class="ud__notice__main ud__notice__main--no-title"><div class="ud__notice__description"><div class="ud__notice__description-content"><span class="text">此浏览器存在兼容性问题会影响你的正常使用，请访问<span class="link"><a href="https://www.feishu.cn/hc/zh-CN/articles/050253926143" target="_blank">帮助中心</a></span>了解更多</span></div></div><button type="button" class="ud__button ud__button--icon ud__button--icon-default ud__button--icon-size-sm ud__notice__close"><span class="universe-icon"><svg width="1em" height="1em" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" data-icon="CloseOutlined"><path d="M20.207 20.207a.99.99 0 0 0 .003-1.403L13.406 12l6.804-6.804a.99.99 0 0 0-.003-1.403.99.99 0 0 0-1.403-.003L12 10.594 5.196 3.79a.99.99 0 0 0-1.403.003.99.99 0 0 0-.003 1.403L10.594 12 3.79 18.804a.99.99 0 0 0 .003 1.403.99.99 0 0 0 1.403.003L12 13.406l6.804 6.804a.99.99 0 0 0 1.403-.003Z" fill="currentColor"></path></svg></span></button></div></div></div>
         dom.hidden = true;
         console.log('not-compatible__announce hidden', dom)
     });
@@ -45,6 +46,7 @@
                     }
                 }
             });
+            
 
             observer.observe(document.body, {
                 childList: true,
