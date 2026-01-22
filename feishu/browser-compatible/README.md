1abe70e29933b79e9aa25cf4919ff54f080b2b96
@刘羡鱼 <evlicn.ok@gmail.com>
2026-01-22 07:16:36 +0000

    feat: add feishu browser compatible

---
+++ feishu/browser-compatible/README.md
@@ -0,0 +1,3 @@
+屏蔽飞书文档浏览器兼容提醒
+
+from: https://greasyfork.org/zh-CN/scripts/460903-%E9%A3%9E%E4%B9%A6%E6%96%87%E6%A1%A3-%E7%9C%9F%E7%9A%84%E5%85%BC%E5%AE%B9%E6%88%91%E7%9A%84%E6%B5%8F%E8%A7%88%E5%99%A8-feishu-doc-is-really-compatible-with-my-browser
---
+++ feishu/browser-compatible/user.js
@@ -0,0 +1,55 @@
+// ==UserScript==
+// @name         【飞书文档】真的兼容我的浏览器 | Feishu doc is really compatible with my browser
+// @namespace    http://tampermonkey.net/
+// @version      0.2
+// @description  隐藏“浏览器不兼容”提示 | Hide "not compatible" banner
+// @author       lideming
+// @match        https://*.feishu.cn/docx/*
+// @match        https://*.feishu.cn/drive/*
+// @match        https://*.feishu.cn/wiki/*
+// @icon         https://www.feishu.cn/favicon.ico
+// @license      MIT
+// @downloadURL https://raw.githubusercontent.com/ybbapp/userscript/feishu/browser-compatible/user.js
+// @updateURL https://raw.githubusercontent.com/ybbapp/userscript/feishu/browser-compatible/user.js
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    waitForElm('.not-compatible__announce').then(dom => {
+        // dom.querySelector('.not-compatible__announce .ud__notice__close').click();
+        dom.hidden = true;
+        console.log('not-compatible__announce hidden', dom)
+    });
+
+    function waitForElm(selector) {
+        return new Promise(resolve => {
+            if (document.querySelector(selector)) {
+                return resolve(document.querySelector(selector));
+            }
+
+            let found = false;
+            const timer = setTimeout(() => {
+                observer.disconnect();
+                console.error("waitForElm timeout");
+                // not resolving/rejecting
+            }, 30000); // ms
+            const observer = new MutationObserver(mutations => {
+                for (const m of mutations) {
+                    for (const node of m.addedNodes) {
+                        if (node.matches(selector)) {
+                            clearTimeout(timer);
+                            found = true;
+                            observer.disconnect();
+                            return resolve(node);
+                        }
+                    }
+                }
+            });
+
+            observer.observe(document.body, {
+                childList: true,
+                subtree: true
+            });
+        });
+    }
+})();
